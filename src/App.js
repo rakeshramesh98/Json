4313a85e2694d3475b8304844cc31e21975c5e0a
rakeshramesh98 <rakesh98raji@gmail.com>
2021-03-01 12:51:13 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import './styles.scss'
 import 'antd/dist/antd.css';
 import getJsonIterable from './utils.js'
 function App() {
@@ -21,21 +20,31 @@ function App() {
     },
   };
 const x=getJsonIterable(data, 0, "", "data")
-
+//console.log(x)
   return (
     <div className="main">
       {x.map((value)=>{
-        value.pop()
+        const lastItem = value[value.length - 1]
+        if(lastItem=="]"||lastItem=="}"){
+          value.splice(-2, 1);
+        }
+        else{
+          value.pop()
+        }
+        
         return(
           <div className="outer">
            {value.map((value2,i)=>{
-             if(value2===""){
-               return <span className="empty-span"></span>
+              if(value2===""){
+                return <span className="empty-span"></span>
+              }
+              else if(value2==="{"||value2==="["){
+                return <span>:{value2}</span>
+              }
+              else if(value2==="}"||value2==="}"){
+               return <span>{value2}</span>
              }
-             else if(value2==="{"||value2==="["){
-               return <span>:{value2}</span>
-             }
-                return <li className="inner"><span className={`class${i}`}>{value2}</span></li>
+                 return <li className="inner"><span className={`class${i}`}>{value2}</span></li>
             })}
           </div>
         )
